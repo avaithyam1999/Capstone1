98d0c6f62178eafd41bff4a6a9d2113d2f0cf17d
AJ V <avaithyam@yahoo.com>
2025-10-14 09:53:19 -0400

    skeleton menu

--- src/main/java/LedgerApp.java
+++ src/main/java/LedgerApp.java
@@ -1,5 +1,41 @@
+import java.util.Scanner;
+
 public class LedgerApp {
     public static void main(String[] args) {
-        System.out.println("this is where my capstone will be!");
+        Scanner scanner = new Scanner(System.in);
+        double accountTotal = 0;
+        boolean running = true;
+
+
+        System.out.println("Welcome to your personal Accounting App!");
+        while (running) {
+            System.out.println("""
+                    Please select an action:
+                    1. Add Deposit
+                    2. Make Payment
+                    3. View Ledger
+                    4. Exit
+                    """);
+            byte userOption = scanner.nextByte();
+            switch (userOption) {
+                case 1 -> {
+                    System.out.println("Please enter the amount for your deposit: ");
+                    double userDepositAmount = scanner.nextDouble();
+
+                    accountTotal += userDepositAmount;
+                    System.out.println("Your updated deposit amount is: $" + accountTotal);
+                }
+                case 2 -> {
+                    System.out.println("you selected make payment");
+                }
+                case 3 -> {
+                    System.out.println("u wanna see deez(ledger)");
+                }
+                case 4 -> {
+                    System.out.println("Thank you for using your Personal Accounting App.\nGoodBye!");
+                    running = false;
+                }
+            }
+        }
     }
 }
